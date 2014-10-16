6535a6b0e5bff47ca813b8d31ed496aff8a586d1
Gregory R. Lee <gregory.lee@cchmc.org>
2014-10-16 18:57:44 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+''' 
+Installation script for pyvolplot
+
+Note:
+To make a source distribution:
+python setup.py sdist
+
+To make an RPM distribution:
+python setup.py bdist_rpm
+
+To Install:
+python setup.py install --prefix=/usr/local
+
+See also:  
+python setup.py bdist --help-formats
+'''
+
+from distutils.core import setup
+
+setup(name='pyvolplot',
+      version='0.1',
+      description='Volume Plotting Utilities',
+      author='Gregory Lee',
+      author_email='grlee77@gmail.com',
+      url='https://github.com/grlee77/pyvolplot',
+      packages=['pyvolplot'],
+     )
+	
