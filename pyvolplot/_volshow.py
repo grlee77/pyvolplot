bad9fe4657f5b8b07f8ff130b2be4dab53ea0723
Gregory R. Lee <grlee77@gmail.com>
2017-11-28 13:54:05 -0500

    fix import for matplotlib 2.1.0

--- pyvolplot/_volshow.py
+++ pyvolplot/_volshow.py
@@ -14,7 +14,11 @@ from .centerplanes_stack import centerplanes_stack, centerplanes_stack_RGB
 from .mips import calc_mips
 from .utils import add_inner_title
 from .overlay import masked_overlay
-from matplotlib import is_string_like
+try:
+    from matplotlib import is_string_like
+except ImportError:
+    # matplotlib 2.1
+    from matplotlib.cbook import is_string_like
 
 # Note: visvis also has function called volshow, but that one is for displaying
 # 3D volumes using OpenGL
